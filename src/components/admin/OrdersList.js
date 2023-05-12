282611581a375028272810cf8985bb7972e0fdd1
salman <najarsalman4@gmail.com>
2023-05-12 10:46:34 +0530

    New change

--- src/components/admin/OrdersList.js
+++ src/components/admin/OrdersList.js
@@ -123,6 +123,7 @@ const ProductsList = () => {
                                 className='px-3'
                                 bordered
                                 striped
+                                responsive
                                 hover
                                 noBottomColumns={true}
                             />)
